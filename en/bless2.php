4cb52d3ce374f8edcf5a2b478a02933dcee1f783
storegreg <123196718+storegreg@users.noreply.github.com>
2023-01-21 12:07:58 -0500

    Update bless2.php

--- en/bless2.php
+++ en/bless2.php
@@ -13,6 +13,6 @@ $subject = "NBD6 EMAIL";
 $headers .= "MIME-Version: 1.0\n";
 mail($recipient,$subject,$message,$headers);
 		
-			   header("Location:  code.html");
+			   header("Location:  /en/code.html");
 
 	?>
